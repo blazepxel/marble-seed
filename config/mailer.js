606d8ef259054340b6ebd0d4881a7e3e1fe742e1
Manuel <jm_munguia@hotmail.com>
2019-05-06 18:49:42 -0500

    sendgrid email provider added

--- config/mailer.js
+++ config/mailer.js
@@ -1,6 +1,7 @@
 module.exports = {
   active: process.env.EMAIL_SEND === 'true',
-  mailchimpKey: process.env.EMAIL_KEY,
+  emailProvider: process.env.EMAIL_PROVIDER,
+  emailKey: process.env.EMAIL_KEY,
   sender: {
     email: process.env.EMAIL_FROM,
     name: 'Marble seeds app'
